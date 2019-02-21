2123f1ec1333a236d83fb7fa4ee141aa6d286d8b
zcx <zhangchixin@>
2019-02-21 09:31:02 +0800

    添加进本地仓库

---
+++ src/main/java/com/itheima/git.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+    public void say(){
+        System.out.println("say something!");
+    }
+}
